b981438d7bb4e6dab2dc5166ee614f87fe6846f3
G <a.gumeniuc@gmail.com>
2024-07-08 12:09:23 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+const myVariable = process.env.MY_URL;
+console.log('The value of MY_URL is:', myVariable);
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
